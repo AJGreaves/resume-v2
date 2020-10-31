19819f564159b20f00390cb5c5a1bff810b822ba
Anna Greaves <gilhespy.anna@gmail.com>
2020-10-31 19:24:09 +0100

    Add feedback for user on contact form submission.

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,7 +1,13 @@
-window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
+window.onload = function () {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault();
         // "gmail" is service ID, "template_ihdtu89" is the template ID
-        emailjs.sendForm('gmail', 'template_ihdtu89', this);
+        emailjs.sendForm('gmail', 'template_ihdtu89', this)
+            .then(function (response) {
+                alert("Your message was sent successfully!");
+                location.reload();
+            }, function (error) {
+                alert("Ooops, something went wrong...", error);
+            });
     });
 }
