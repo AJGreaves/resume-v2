67f7746e0767888c16f0c6b0bcfac43efdd23e01
Anna Greaves <gilhespy.anna@gmail.com>
2020-11-01 15:43:44 +0100

    Add comments to js files and with contact form in html.

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,12 +1,19 @@
 window.onload = function () {
-    document.getElementById('contact-form').addEventListener('submit', function (event) {
+    /**
+     * Adds a submit event listener to #contact-form
+     * Sends the form data to emailjs, adds an alert to the user
+     * for successful message sent, or error.
+     */
+    $('#contact-form').submit(function (event) {
         event.preventDefault();
         // "gmail" is service ID, "template_ihdtu89" is the template ID
         emailjs.sendForm('gmail', 'template_ihdtu89', this)
-            .then(function (response) {
+            .then(function () {
+                // If message sent successfully, alert user and then refresh the page
                 alert("Your message was sent successfully!");
                 location.reload();
             }, function (error) {
+                // If error with sending message, alert user with error message.
                 alert("Ooops, something went wrong...", error);
             });
     });
--- assets/js/map.js
+++ assets/js/map.js
@@ -1,7 +1,7 @@
-/* Set which part of the world the map loads on, and the zoom level */
+// Set which part of the world the map loads on, and the zoom level
 let mymap = L.map('map').setView([52.856, 5.545], 4);
 
-/* Builds map with access token */
+// Builds map with access token
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY29kZWluc3RpdHV0ZSIsImEiOiJja2d4dXgzbXIwbmxvMnJyMXAwMmpuemI4In0.VLKabDve9EvypBPxhLOYCw', {
     attribution: false,
     maxZoom: 18,
@@ -11,12 +11,12 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
 
-/* Sets markers */
+// Sets markers
 let djangoCon = L.marker([55.683610, 12.563650]).addTo(mymap);
 let reactEurope = L.marker([48.856613, 2.352222]).addTo(mymap);
 let euroPython = L.marker([55.953251, -3.188267]).addTo(mymap);
 
-/* Sets contents for popups on markers */
+// Sets contents for popups on markers
 djangoCon.bindPopup("<b>DjangoCon</b><br>København, Denmark").openPopup();
 reactEurope.bindPopup("<b>React Conf</b><br>Paris, France");
 euroPython.bindPopup("<b>EuroPython</b><br>Edinburgh, Scotland ");
